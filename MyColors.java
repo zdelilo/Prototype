89663648c35f497655d0c0fe6c39a9d7c66457f1
Catherine <Catherine@Catherine>
2017-03-31 16:13:08 -0400

    Hso and Elections added

---
+++ MyColors.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class MyColors {
+	/**MY COLORS**/
+   static Color seaGreen = new Color(64,224,208);
+   static Color deepBlue = new Color(70,130,180);
+   static Color beechyBlue = new Color(135,206,235);
+   static Color maroon = new Color(128,0,0);
+   static Color tomato = new Color(255,99,71);
+   static Color paleTurquoise = new Color(175,238,238);
+   static Color gold = new Color(255,215,0);
+   static Color kaki = new Color(255,239,213);
+}
